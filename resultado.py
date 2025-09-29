c9eb6a1d97295a6ea1315cb97f2008ccebea901d
MariaLu07 <malupdeo07@gmail.com>
2025-09-29 00:14:58 -0300

    Create resultado.py

---
+++ resultado.py
@@ -0,0 +1,5 @@
+resultado = input("Qual foi o resultado da missão? ")
+descricao = input("Descreva a missão: ")
+
+print(f"\nResultado da missão: {resultado}")
+print(f"Descrição da missão: {descricao}")
