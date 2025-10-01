d444b4807991125c66772dc95a8d345f963b92dc
MariaLu07 <malupdeo07@gmail.com>
2025-10-01 08:35:49 -0300

    Create perfil.py

---
+++ perfil.py
@@ -0,0 +1,9 @@
+nomecom = input("Digite seu nome completo: ")
+nascimento = input("Digite sua data de nascimento: ")
+genero = input("Qual seu gênero? ")
+cargo = input("Qual o seu cargo?")
+
+print(f"\nSeu nome completo é {nomecom}")
+print(f"Sua data de nascimento é {nascimento}")
+print(f"Seu gênero é {genero}")
+print(f"Seu cargo é: {cargo}")
