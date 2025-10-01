ac19967ca7938559d8aa29382d358c2ef3f09169
MariaLu07 <malupdeo07@gmail.com>
2025-10-01 08:42:48 -0300

    Create login.py

---
+++ login.py
@@ -0,0 +1,10 @@
+nome = input("Digite seu nome de usuário: ")
+email = input("Digite seu email: ")
+senha = input("Digite sua senha: ")
+
+print(f"\nSeu login foi realizado com sucesso")
+print(f"Suas informações estão abaixo:")
+
+print(f"\nSeu nome é: {nome}")
+print(f"Seu email é: {email}")
+print(f"Sua senha é: {senha}")
