1ef635fa81a32170f20e2784d5947878735d8d24
MariaLu07 <malupdeo07@gmail.com>
2025-10-01 08:38:51 -0300

    Create cadastro.py

---
+++ cadastro.py
@@ -0,0 +1,9 @@
+nome = input("Digite seu nome de usuário: ")
+email = input("Digite seu email: ")
+telefone = input("Digite seu telefone: ")
+senha = input("Digite sua senha: ")
+
+print(f"\nSeu nome é: {nome}")
+print(f"Seu email é: {email}")
+print(f"Seu telefone é: {telefone}")
+print(f"Sua senha é: {senha}")
